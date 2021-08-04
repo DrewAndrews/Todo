a66145fd6aaa45d631c60790cf2cf626e2cc2ee0
DrewAndrew <andreyrusinovich1970@gmail.com>
2021-08-04 15:18:21 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Andrey Rusinovich on 04.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
